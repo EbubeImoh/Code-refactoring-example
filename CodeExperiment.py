69a56acb5c42d65f982447a82da8abd32105c3d9
Ebube Imoh <68086697+EbubeImoh@users.noreply.github.com>
2024-07-31 20:13:15 +0100

    Initial code upload

---
+++ CodeExperiment.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import joblib
+import pandas as pd
+
+sample_data = pd.read_csv("C:\\Users\\amanr\\OneDrive\\Desktop\\diabetes.csv")
+
+df = sample_data
+
+X = df.drop(['Outcome'], axis=1)
+y = df['Outcome']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+data = {"train": {"X": X_train, "y": y_train},
+        "test": {"X": X_test, "y": y_test}}  
+
+args = {
+    "alpha": 0.5
+}
+
+reg_model = Ridge(**args)
+reg_model.fit(data["train"]["X"], data["train"]["y"])
+
+preds = reg_model.predict(data["test"]["X"])
+mse = mean_squared_error(preds, y_test)
+metrics = {"mse": mse}
+print(metrics)
+
+model_name = "sklearn_regression_model.pkl"
+joblib.dump(value=reg_model, filename=model_name)
---
+++ train.py
@@ -0,0 +1,64 @@
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import joblib
+import sys
+
+# Loading data
+def load_data(path):
+    sample_data = pd.read_csv(path)
+    return sample_data
+
+# Split the dataframe into test and train data
+def split_data(df):
+    X = df.drop(['Outcome'], axis=1)
+    y = df['Outcome']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+    data = {"train": {"X": X_train, "y": y_train},
+            "test": {"X": X_test, "y": y_test}}
+    return data
+
+# Train the model, return the model
+def train_model(data, args):
+    reg_model = Ridge(**args)
+    reg_model.fit(data["train"]["X"], data["train"]["y"])
+    return reg_model
+
+# Evaluate the metrics for the model
+def get_model_metrics(reg_model, data):
+    preds = reg_model.predict(data["test"]["X"])
+    mse = mean_squared_error(preds, data["test"]["y"])
+    metrics = {"mse": mse}
+    return metrics
+
+def main():
+    # Load Data
+    sample_data = load_data(sys.argv[1])
+
+    df = sample_data
+    
+    # Split Data into Training and Validation Sets
+    data = split_data(df)
+
+    # Train Model on Training Set
+    args = {
+        "alpha": 0.5
+    }
+    reg = train_model(data, args)
+
+    # Validate Model on Validation Set
+    metrics = get_model_metrics(reg, data)
+
+    # Save Model
+    model_name = "sklearn_regression_model.pkl"
+
+    joblib.dump(value=reg, filename=model_name)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print("An unknown exception occured")
